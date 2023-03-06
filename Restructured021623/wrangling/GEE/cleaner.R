5790fc05a6b3b8ffe0f4e991750430c94494f5e7
TJ Sipin <tjsipin@TJs-MacBook-Air.local>
2023-03-05 20:29:16 -0800

    processed; need to fix, scales wrong;

--- Restructured021623/wrangling/GEE/cleaner.R
+++ Restructured021623/wrangling/GEE/cleaner.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(tidyr)
 # Pulls together all variables into one data set using neighborhoods of zones with population > 1 person/km^2
 ## Variables
 ### Average Radiance
@@ -23,6 +24,7 @@ library(dplyr)
 # each variable together
 
 
+
 cleanerFunc <- function(data, country, var){
   cleaned <- data %>%
     select(-c(system.index, .geo)) %>%
@@ -39,7 +41,7 @@ cleanerFunc <- function(data, country, var){
   } else {
     cleaned_v2 <- cleaned %>%
       select(-date, -Day) %>%
-      group_by(Year, Month, MuniCode, Country) %>%
+      group_by(Year, MuniCode, Month, Country) %>%
       summarise_all(mean) %>%
       ungroup()
   }
@@ -57,7 +59,7 @@ cleanerFunc_precip <- function(data, country){
            Country = country)
 
   cleaned_v2 <- cleaned %>%
-    dplyr::select(-date) %>%
+    dplyr::select(-date, -Day) %>%
     group_by(Year, Month, MuniCode, Country) %>%
     summarise_all(sum) %>%
     ungroup()
@@ -76,22 +78,42 @@ cleanerFunc_precip <- function(data, country){
 
 
 raw_colombia_files <- list.files("~/peregrine_amazon/data/colombia/raw", full.names = F)
-
-
-cleanCSVfunc_colombia <- function(file_name){
-  data <- read.csv(file = paste0("~/peregrine_amazon/data/colombia/raw/", file_name))
-  country = "Colombia"
-  data_str <- file_name
-  cleaned <- if(grepl("precip", file_name, ignore.case = T)){
-    cleanerFunc_precip(data, country)
-  } else {
-    cleanerFunc(data, country)
-  }
-  if("mean" %in% names(cleaned)){
-    colnames(cleaned)[colnames(cleaned) == "mean"] <- data_str # rename after
-  }
-  write.csv(cleaned, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed.csv", data_str)))
+cleanCSVfunc_colombia_toRDS(raw_colombia_files[1])
+clean_colombia_veg <- readRDS("~/peregrine_amazon/data/colombia/processed/full_colombia_modis_veg_processed")
+clean_colombia_hntl <- readRDS("~/peregrine_amazon/data/colombia/processed/full_colombia_HNTL_processed")
+##
+file_name = raw_colombia_files[1]
+data <- read.csv(file = paste0("~/peregrine_amazon/data/colombia/raw/", file_name))
+country = "Colombia"
+data_str <- file_name
+cleaned <- if(grepl("precip", file_name, ignore.case = T)){
+  cleanerFunc_precip(data, country)
+} else {
+  cleanerFunc(data, country)
 }
+
+if("mean" %in% names(cleaned)){
+  colnames(cleaned)[colnames(cleaned) == "mean"] <- data_str # rename after
+}
+
+if("Month" %in% names(cleaned)){
+  saveRDS(cleaned, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed_monthly", data_str)))
+  annual <- cleaned %>%
+    group_by(Year, MuniCode, Country) %>%
+    select(-Month) %>%
+    summarise_all(mean, na.rm = ifelse(sum(complete.cases(.)) > 10, T, F)) %>%
+    ungroup()
+  saveRDS(annual, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+} else {
+  saveRDS(cleaned, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+}
+
+
+
+
+
+
+##
 
 cleanCSVfunc_colombia_toRDS <- function(file_name){ # save space
   data <- read.csv(file = paste0("~/peregrine_amazon/data/colombia/raw/", file_name))
@@ -102,10 +124,22 @@ cleanCSVfunc_colombia_toRDS <- function(file_name){ # save space
   } else {
     cleanerFunc(data, country)
   }
+
   if("mean" %in% names(cleaned)){
     colnames(cleaned)[colnames(cleaned) == "mean"] <- data_str # rename after
   }
-  saveRDS(cleaned, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed", data_str)))
+
+  if("Month" %in% names(cleaned)){
+    saveRDS(cleaned, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed_monthly", data_str)))
+    annual <- cleaned %>%
+      group_by(Year, MuniCode, Country) %>%
+      select(-Month) %>%
+      summarise_all(mean, na.rm = ifelse(sum(complete.cases(.)) > 10, T, F)) %>%
+      ungroup()
+    saveRDS(annual, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+  } else {
+    saveRDS(cleaned, paste0("~/peregrine_amazon/data/colombia/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+  }
 }
 
 
@@ -126,22 +160,6 @@ for(file in raw_colombia_files){
 
 raw_peru_files <- list.files("~/peregrine_amazon/data/peru/raw", full.names = F)
 
-
-cleanCSVfunc_peru <- function(file_name){
-  data <- read.csv(file = paste0("~/peregrine_amazon/data/peru/raw/", file_name))
-  country = "Peru"
-  data_str <- file_name
-  cleaned <- if(grepl("precip", file_name, ignore.case = T)){
-    cleanerFunc_precip(data, country)
-  } else {
-    cleanerFunc(data, country)
-  }
-  if("mean" %in% names(cleaned)){
-    colnames(cleaned)[colnames(cleaned) == "mean"] <- data_str # rename after
-  }
-  write.csv(cleaned, paste0("~/peregrine_amazon/data/peru/processed/", gsub("_raw.csv", "_processed.csv", data_str)))
-}
-
 cleanCSVfunc_peru_toRDS <- function(file_name){ # save space
   data <- read.csv(file = paste0("~/peregrine_amazon/data/peru/raw/", file_name))
   country = "Peru"
@@ -151,11 +169,25 @@ cleanCSVfunc_peru_toRDS <- function(file_name){ # save space
   } else {
     cleanerFunc(data, country)
   }
+
   if("mean" %in% names(cleaned)){
     colnames(cleaned)[colnames(cleaned) == "mean"] <- data_str # rename after
   }
-  saveRDS(cleaned, paste0("~/peregrine_amazon/data/peru/processed/", gsub("_raw.csv", "_processed", data_str)))
+
+  if("Month" %in% names(cleaned)){
+    saveRDS(cleaned, paste0("~/peregrine_amazon/data/peru/processed/", gsub("_raw.csv", "_processed_monthly", data_str)))
+    annual <- cleaned %>%
+      group_by(Year, MuniCode, Country) %>%
+      select(-Month) %>%
+      summarise_all(mean, na.rm = ifelse(sum(complete.cases(.)) > 10, T, F)) %>%
+      ungroup()
+    saveRDS(annual, paste0("~/peregrine_amazon/data/peru/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+  } else {
+    saveRDS(cleaned, paste0("~/peregrine_amazon/data/peru/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+  }
 }
+
+
 
 for(file in raw_peru_files){
   print(file)
@@ -172,20 +204,6 @@ for(file in raw_peru_files){
 
 raw_brazil_files <- list.files("~/peregrine_amazon/data/brazil/raw", full.names = F) # remove population
 
-cleanCSVfunc_brazil <- function(file_name){
-  data <- read.csv(file = paste0("~/peregrine_amazon/data/brazil/raw/", file_name))
-  country = "Brazil"
-  data_str <- file_name
-  cleaned <- if(grepl("precip", file_name, ignore.case = T)){
-    cleanerFunc_precip(data, country)
-  } else {
-    cleanerFunc(data, country)
-  }
-  if("mean" %in% names(cleaned)){
-    colnames(cleaned)[colnames(cleaned) == "mean"] <- data_str # rename var
-  }
-  write.csv(cleaned, paste0("~/peregrine_amazon/data/brazil/processed/", gsub("_raw.csv", "_processed.csv", data_str)))
-}
 
 cleanCSVfunc_brazil_toRDS <- function(file_name){ # save space
   data <- read.csv(file = paste0("~/peregrine_amazon/data/brazil/raw/", file_name))
@@ -196,10 +214,22 @@ cleanCSVfunc_brazil_toRDS <- function(file_name){ # save space
   } else {
     cleanerFunc(data, country)
   }
+
   if("mean" %in% names(cleaned)){
     colnames(cleaned)[colnames(cleaned) == "mean"] <- data_str # rename after
   }
-  saveRDS(cleaned, paste0("~/peregrine_amazon/data/brazil/processed/", gsub("_raw.csv", "_processed", data_str)))
+
+  if("Month" %in% names(cleaned)){
+    saveRDS(cleaned, paste0("~/peregrine_amazon/data/brazil/processed/", gsub("_raw.csv", "_processed_monthly", data_str)))
+    annual <- cleaned %>%
+      group_by(Year, MuniCode, Country) %>%
+      select(-Month) %>%
+      summarise_all(mean, na.rm = ifelse(sum(complete.cases(.)) > 10, T, F)) %>%
+      ungroup()
+    saveRDS(annual, paste0("~/peregrine_amazon/data/brazil/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+  } else {
+    saveRDS(cleaned, paste0("~/peregrine_amazon/data/brazil/processed/", gsub("_raw.csv", "_processed_annual", data_str)))
+  }
 }
 
 
@@ -210,29 +240,164 @@ for(file in raw_brazil_files){
   cleanCSVfunc_brazil_toRDS(file)
 }
 
+
 #################################################################
 ##                       Stitch together                       ##
 #################################################################
 
 # Colombia
 
-processed_colombia_files <- list.files("~/peregrine_amazon/data/colombia/processed/", full.names = T)
-full_colombia_env_vars <- data.frame(NA)
+## use to filter to munis of interest;
+## future task might be to filter colombia shapefile starting from GEE
+colombia_munis_filter <- read.csv("~/MacDonald-REU-Summer-22-1/models/data/aad.csv") %>%
+  filter(Country == 'Colombia') %>%
+  filter(!is.na(pland_forest)) %>%
+  select(Code) %>%
+  unique()
+
+processed_colombia_files <- list.files("~/peregrine_amazon/data/colombia/processed/", full.names = T, pattern = "_annual")
+
+sapply(sapply(processed_colombia_files, readRDS), names)
+
+full_colombia_env_vars <- data.frame(MuniCode = NA, Year = NA, Country = NA, stringsAsFactors = F)
 for(file in processed_colombia_files){
   print(file)
-  data <- read.csv(file)
-  full_colombia_env_vars = full_join(full_colombia_env_vars, data)
+  data <- readRDS(file)
+  full_colombia_env_vars = full_join(full_colombia_env_vars, data, by = c('MuniCode', 'Year', 'Country'))
 }
 
+full_colombia_env_vars_v2 <- full_colombia_env_vars %>%
+  filter(Year > 2000) %>%
+  select(-Day) %>%
+  rename(Code = MuniCode,
+         HNTL = full_colombia_HNTL_raw.csv,
+         LST_Day = LST_Day_1km,
+         LST_Night = LST_Night_1km,
+         Precip = full_colombia_Precip_raw.csv,
+         StableLights = full_colombia_StableLights_raw.csv,
+         AvgRad = full_colombia_AvgRad_raw.csv) %>%
+  filter(Code %in% colombia_munis_filter$Code) %>%
+  select(Code, Year, Country, NDVI, EVI, LST_Day, LST_Night,
+         HNTL, StableLights, AvgRad, Precip,
+         Evap_tavg, Qair_f_tavg, SoilMoi00_10cm_tavg, Wind_f_tavg,
+         Tair_f_tavg, everything())
+
+full_colombia_env_vars_v2 %>% summary()
 
 
-read.csv(processed_colombia_files[3]) %>% names
+## PERU ##
+
+peru_munis_filter <- read.csv("~/MacDonald-REU-Summer-22-1/models/data/aad.csv") %>%
+  filter(Country == 'Peru') %>%
+  filter(!is.na(pland_forest)) %>%
+  select(Code) %>%
+  unique()
+
+processed_peru_files <- list.files("~/peregrine_amazon/data/peru/processed/", full.names = T, pattern = "_annual")
+
+sapply(sapply(processed_peru_files, readRDS), names)
+
+full_peru_env_vars <- data.frame(MuniCode = NA, Year = NA, Country = NA, stringsAsFactors = F)
+for(file in processed_peru_files){
+  print(file)
+  data <- readRDS(file)
+  full_peru_env_vars = full_join(full_peru_env_vars, data, by = c('MuniCode', 'Year', 'Country'))
+}
+
+full_peru_env_vars_v2 <- full_peru_env_vars %>%
+  filter(Year > 2000) %>%
+  select(-Day) %>%
+  rename(Code = MuniCode,
+         HNTL = full_peru_HNTL_raw.csv,
+         LST_Day = LST_Day_1km,
+         LST_Night = LST_Night_1km,
+         Precip = `full_peru_precip_20-21_raw.csv`,
+         AvgRad = full_peru_AvgRad_raw.csv) %>%
+  filter(Code %in% peru_munis_filter$Code) %>%
+  mutate(StableLights = NA) %>%
+  select(Code, Year, Country, NDVI, EVI, LST_Day, LST_Night,
+         HNTL, StableLights, AvgRad, Precip,
+         Evap_tavg, Qair_f_tavg, SoilMoi00_10cm_tavg, Wind_f_tavg,
+         Tair_f_tavg, everything())
 
 
-processed_peru_files <- list.files("~/peregrine_amazon/data/peru/processed/", full.names = T)
+## Brazil ##
 
-read.csv(processed_peru_files[1]) %>% names()
 
-processed_colombia_files <- list.files("~/peregrine_amazon/data/colombia/processed/", full.names = T)
+## use to filter to munis of interest;
+## future task might be to filter brazil shapefile starting from GEE
+brazil_munis_filter <- read.csv("~/MacDonald-REU-Summer-22-1/models/data/aad.csv") %>%
+  filter(Country == 'Brazil') %>%
+  filter(!is.na(pland_forest)) %>%
+  select(Code) %>%
+  unique()
 
-read.csv(processed_colombia_files[3]) %>% names()
+processed_brazil_files <- list.files("~/peregrine_amazon/data/brazil/processed/", full.names = T, pattern = "_annual")
+
+sapply(sapply(processed_brazil_files, readRDS), summary)
+sapply(sapply(processed_brazil_files, readRDS), dim)
+
+full_brazil_env_vars <- data.frame(MuniCode = NA, Year = NA, Country = NA, stringsAsFactors = F)
+for(file in processed_brazil_files){
+  print(file)
+  data <- readRDS(file)
+  full_brazil_env_vars = full_join(full_brazil_env_vars, data, by = c('MuniCode', 'Year', 'Country'))
+}
+
+full_brazil_env_vars_v2 <- full_brazil_env_vars %>%
+  filter(Year > 2000) %>%
+  select(-Day) %>%
+  rename(Code = MuniCode,
+         HNTL = full_brazil_HNTL_raw.csv,
+         LST_Day = LST_Day_1km,
+         LST_Night = LST_Night_1km,
+         Precip = `full_brazil_precip_20-21_raw.csv`,
+         AvgRad = full_brazil_AvgRad_raw.csv) %>%
+  filter(Code %in% brazil_munis_filter$Code) %>%
+  mutate(StableLights = NA) %>%
+  select(Code, Year, Country, NDVI, EVI, LST_Day, LST_Night,
+         HNTL, StableLights, AvgRad, Precip,
+         Evap_tavg, Qair_f_tavg, SoilMoi00_10cm_tavg, Wind_f_tavg,
+         Tair_f_tavg, everything())
+
+full_brazil_env_vars_v2 %>% summary()
+
+full_env_vars_updated <- full_colombia_env_vars_v2 %>%
+  rbind(full_peru_env_vars_v2) %>%
+  rbind(full_brazil_env_vars_v2) %>%
+  group_by(Country, Year) %>%
+  arrange(Code) %>%
+  # group_by(Code, .add=T) %>%
+  # mutate(n = sum(complete.cases(.)/n())) %>%
+  ungroup()
+
+## aad to merge ##
+aad <- read.csv("~/MacDonald-REU-Summer-22-1/models/data/aad.csv")
+
+aad_v2 <- aad %>%
+  full_join(full_env_vars_updated, by = c("Code", "Year", "Country",
+                                          'NDVI', 'EVI', 'LST_Day', 'LST_Night',
+                                          'StableLights', 'AvgRad', 'Precip')) %>% # Use fill()
+  mutate(Country = as.factor(Country)) %>%
+  filter(Code %in% intersect(aad$Code, full_env_vars_updated$Code))
+
+aad_v2 %>% summary()
+
+aad_v2 %>% filter(Country == "Colombia") %>% View("colombia")
+aad_v2 %>% filter(Country == "Brazil") %>% View("brazil")
+aad_v2 %>% filter(Country == "Peru") %>% View("peru")
+
+aad_v3 <- aad_v2 %>%
+  group_by(Country) %>%
+  arrange(Code, Year)
+
+aad_v3 %>% filter(Country == "Colombia") %>% View("colombia")
+aad_v2 %>% filter(Country == "Brazil") %>% View("brazil")
+aad_v2 %>% filter(Country == "Peru") %>% View("peru")
+
+
+## Missing
+
+naniar::gg_miss_var(aad_v2 %>%
+                      select(-c(Chikungunya:Zika, OptTemp_Obs:Malaria_OptTemp)),
+                    facet = Year)
